3bb21b204c2afe2425797617e1238a0fea0ceabb
Ashutosh <ashutosh.kumar.patel.2003@gmail.com>
2023-11-25 23:26:46 +0530

    Add upload middleware and isValidUserToken

--- backend/routes/eventRouter/event.js
+++ backend/routes/eventRouter/event.js
@@ -2,10 +2,13 @@ const express = require("express");
 const router = express.Router();
 
 const { catchErrors } = require("@/handlers/errorHandlers");
-
+const { isValidUserToken } = require("../../middleware/isValidUserToken");
 const { birthdayController, formFieldController } = require("@/controllers/eventController");
+const uploadMiddleware = require("@/middleware/uploadMiddleware");
 
-router.route('/birthday/create').post(catchErrors(birthdayController.create));
+const uploadPosterPic = uploadMiddleware('./public/uploads/posterImage').single('posterPath');
+
+router.route('/birthday/create').post(isValidUserToken, uploadPosterPic, catchErrors(birthdayController.create));
 router.route('/birthday/get/:id').get(catchErrors(birthdayController.read));
 router.route('/birthday/get/').get(catchErrors(formFieldController));
 router.route('/birthday/update/:id').put(catchErrors(birthdayController.update));
