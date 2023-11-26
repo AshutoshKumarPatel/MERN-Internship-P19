f15b45b2611201a451da7caa13de5e21dbd31d5d
Ashutosh <ashutosh.kumar.patel.2003@gmail.com>
2023-11-27 00:53:45 +0530

    Add index, layout and css

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,7 +6,12 @@ import Layout from './Layout.jsx'
 import Home from './components/Home/Home.jsx'
 import Login from './components/Login/Login.jsx'
 import Signup from './components/Signup/Signup.jsx'
-import Birthday from './components/Birthday/Birthday.jsx'
+import EventCreate from './components/Events/EventCreate.jsx/'
+import AdditionalUserForm from './components/UserForms/AdditionalUserForm.jsx'
+import EditForm from './components/UserForms/EditUserForm.jsx'
+import Profile from './components/Profile/Profile'
+import Dashboard from './components/Dashboard/Dashboard'
+import EventEdit from './components/Events/EventEdit'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
@@ -14,7 +19,17 @@ const router = createBrowserRouter(
       <Route path='/' element={<Home />}/>
       <Route path='/login' element={<Login />}/>
       <Route path='/signup' element={<Signup />}/>
-      <Route path='/birthday' element={<Birthday />}/>
+      <Route path='/profile' element={<Profile />}/>
+      <Route path='/user/additionalForm' element={<AdditionalUserForm />}/>
+      <Route path='/user/edit' element={<EditForm />}/>
+      <Route path='/event/birthday/:model' element={<EventCreate />}/>
+      <Route path='/event/concert/:model' element={<EventCreate />}/>
+      <Route path='/event/wedding/:model' element={<EventCreate />}/>
+      <Route path='/event/corporate/:model' element={<EventCreate />}/>
+      <Route path='/event/educational/:model' element={<EventCreate />}/>
+      <Route path='/event/custom/:model' element={<EventCreate />}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
+      <Route path='/event/edit/:id/:model' element={<EventEdit/>}/>
     </Route>
   )
 )
